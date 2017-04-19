25f6693cdd4f7c3caab5fb864f3656f14a04d38c
Evan Cooper <evan.cooper@rogers.com>
2017-04-19 13:00:37 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-server_address = '99.240.202.116:9920'
+server_address = 'server_ip'
 domain_address = 'door.evancooper.tech'
 PORT = 9920
 archive_log_prefix = 'logs/archive/'
 sensor_log_file = 'logs/sensor_log.txt'
 server_log_file = 'logs/server_log.txt'
 html_file = 'html/index.html'
-users = {'Evan':'1234'}
+users = {'usr':'psw'}
 door_open = False
 
 valid_requests = [
